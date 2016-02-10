db848ab7a10efe3b66a9e1a2454ea54071e8fecc
LibViz <libviz@usc-secure-wireless-063-054.usc.edu>
2016-02-10 15:43:10 -0800

    code cleanup in databaseManager

--- Assets/Resources/Scripts/DatabaseManager.cs
+++ Assets/Resources/Scripts/DatabaseManager.cs
@@ -6,13 +6,6 @@ using System.Collections.Generic;
 
 public class DatabaseManager : MonoBehaviour {
 
-	[SerializeField] GameObject login;
-	[SerializeField] GameObject Navi;
-	[SerializeField] GameObject[] fields;
-	[SerializeField] GameObject bookInfoBarPrefab;
-	[SerializeField] float barHeight = 35f;
-	[SerializeField] Image black;
-
 	[SerializeField] SQLConnector connector {get { return SQLConnector.Instance;}}
 
 	public void Login() {
@@ -25,95 +18,45 @@ public class DatabaseManager : MonoBehaviour {
 		}
 	}
 
-
-	[SerializeField] InputField search;
-	[SerializeField] RectTransform searchInfo;
-	public void Search(List<BookInfo> inputInfos)
+	public List<BookInfo> Search(List<BookInfo> inputInfos)
 	{
-		List<BookInfo> infos = connector.Search(search.text);
+		List<BookInfo> outputInfos = connector.Search(inputInfos);
+		print(inputInfos.Count + " " + outputInfos.Count);
+		return outputInfos;
 	}
 
-	[SerializeField] InputField advanceSearch;
- 	[SerializeField] Dropdown advanceDropdown;
-	public void AdvancedSearch()
+	public List<BookInfo> AdvancedSearch(int dropdownValue, string searchText)
 	{
 		List<BookInfo> infos = new List<BookInfo>();
-		if (advanceDropdown.value <= 3)
+		if (dropdownValue <= 3)
 		{
 			string field = "";
-			if (advanceDropdown.value == 0)
+			if (dropdownValue == 0)
 				field = "Title";
-			if (advanceDropdown.value == 1)
+			if (dropdownValue == 1)
 				field = "Name";
-			if (advanceDropdown.value == 2)
+			if (dropdownValue == 2)
 				field = "Location";
-			if (advanceDropdown.value == 3)
+			if (dropdownValue == 3)
 				field = "Description";
-			infos = connector.Search(advanceSearch.text, field );
+			infos = connector.Search(searchText, field );
 		}
-		if (advanceDropdown.value == 4)
+		if (dropdownValue == 4)
 		{
-			if (advanceSearch.text.EndsWith("-"))
-				infos = connector.Search( Global.IntFilter( advanceSearch.text) , "Time" , -1);	
+			if (searchText.EndsWith("-"))
+				infos = connector.Search( Global.IntFilter(searchText) , "Time" , -1);
 			else
-			if (advanceSearch.text.EndsWith("+"))
-				infos = connector.Search( Global.IntFilter( advanceSearch.text) , "Time" , 1);	
+			if (searchText.EndsWith("+"))
+				infos = connector.Search( Global.IntFilter(searchText) , "Time" , 1);
 			else
-				infos = connector.Search( Global.IntFilter( advanceSearch.text) , "Time" , 0);	
-		}
-	}
-
-	[SerializeField] InputField bookSearch;
-	public void SearchBook()
-	{
-		List<BookInfo> infos = connector.Search( connector.Search(bookSearch.text,"Title")[0]);
-	}
-
-
-	[SerializeField] InputField tagSearch;
-	public void SearchTag()
-	{
-		List<BookInfo> infos = connector.SearchByTag( tagSearch.text );
-	}
-
-	public void RemoveAllChild(Transform root )
-	{
-		foreach(Transform t in root)
-		{
-			Destroy(t.gameObject);
-		}
-	}
-
-	public void CreateBookInfoBar(RectTransform parent, int index , BookInfo info = null)
-	{
-		GameObject obj = Instantiate(bookInfoBarPrefab) as GameObject;
-		RectTransform recTrans = obj.GetComponent<RectTransform>();
-		recTrans.parent = parent;
-		Vector3 pos = Vector3.zero;
-		pos.y = - index * barHeight;
-		recTrans.localPosition = pos;
-
-		Text[] texts = obj.GetComponentsInChildren<Text>();
-		foreach(Text t in texts)
-		{
-			t.DOFade(0, 1f).From().SetDelay(0.2f * index);
-			if (info != null)
-			{
-				if (t.name == "Title")
-					t.text = info.Title;
-				if (t.name == "Author")
-					t.text = info.Author;
-				if (t.name == "Time")
-					t.text = info.Time.ToString();
-				if (t.name == "Note")
-					t.text = info.Note;
-				if (t.name == "Genre")
-					t.text = info.genre+'-'+info.topical_term;
-				if (t.name == "Location")
-					t.text = info.Location;
-
-			}
+				infos = connector.Search( Global.IntFilter(searchText) , "Time" , 0);
 		}
 
+		return infos;
+	}
+
+	public List<BookInfo> SearchTag(string tag)
+	{
+		return connector.SearchByTag(tag);
 	}
 }
