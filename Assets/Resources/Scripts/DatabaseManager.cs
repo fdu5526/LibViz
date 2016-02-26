3a38650d6caebf03a061716b9f5da2cd9f883767
Dave Yan <fdu5526@live.com>
2016-02-25 17:17:42 -0800

    skeleton set up for querying database

--- Assets/Resources/Scripts/DatabaseManager.cs
+++ Assets/Resources/Scripts/DatabaseManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 using System.Collections;
 using UnityEngine.UI;
 using DG.Tweening;
@@ -19,14 +20,48 @@ public class DatabaseManager : MonoBehaviour {
 		}
 	}
 
-	public List<BookInfo> Search(List<BookInfo> inputInfos)
-	{
+	public List<List<BookInfo>> Search(List<BookInfo> inputInfos) {
 		Debug.Assert(inputInfos != null);
 		Debug.Assert(inputInfos.Count > 0);
 
+		// search by all fields, get all the results
+		int numOfFields = Enum.GetNames(typeof(Field)).Length;
+		for (int i = 0; i < numOfFields; i++) {
+			Field f = (Field)i;
+			switch (f) {
+				case Field.TITLE:
+					break;
+				case Field.AUTHOR:
+					break;
+				case Field.NOTE:
+					break;
+				case Field.TIME:
+					break;
+				case Field.GENRE:
+					break;
+				case Field.TOPICAL_TERM:
+					break;
+				case Field.FORM_SUBDIVISION:
+					break;
+				case Field.GENERAL_SUBDIVISION:
+					break;
+				case Field.CHRONOLOGICAL_SUBDIVISION:
+					break;
+				case Field.GEOGRAPHIC_SUBDIVISION:
+					break;
+				default: // no search by publisher or publishing date
+					break;
+			}
+		}
+
+		// TODO sort the search results
+
 		List<BookInfo> outputInfos = connector.Search(inputInfos);
-		print(inputInfos.Count + " " + outputInfos.Count);
-		return outputInfos;
+		
+		// return the top 5 fields results
+		
+		//TODO
+		return null;
 	}
 
 	public List<BookInfo> AdvancedSearch(int dropdownValue, string searchText)
