8686d6d5a39ff46e95a5054765aca94c7bf3176c
Dave Yan <fdu5526@live.com>
2016-04-02 21:48:01 -0700

    less perspective when zoomed in now due to FOV change

--- Assets/Resources/Scripts/MainCamera.cs
+++ Assets/Resources/Scripts/MainCamera.cs
@@ -1,45 +1,64 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class MainCamera : MonoBehaviour {
 
-	enum State { ZoomIn, ZoomOut, Stop };
+	enum State { ZoomIn, ZoomOut, StoppedIn, StoppedOut };
 	State currentState;
 
 	Vector3 zoomPosition, defaultPosition;
+	float defaultFov, zoomFov;
+
+	Timer transitionTimer;
 	
 	// Use this for initialization
 	void Start () {
-		currentState = State.Stop;
-		defaultPosition = new Vector3(0f, 9.97f, -20.04f);
-		zoomPosition = new Vector3(0f, 8.6f, -18.02f);
+		currentState = State.StoppedOut;
+		defaultPosition = new Vector3(0f, 9.4f, -20.04f);
+		defaultFov = 60f;
+
+		zoomPosition = new Vector3(0f, 8.6f, -28.93f);
+		zoomFov = 40f;
+
+		GetComponent<Camera>().fieldOfView = defaultFov;
+		transform.position = defaultPosition;
+
+		transitionTimer = new Timer(2f);
 	}
 
 
 	public void ZoomIn () {
-		currentState = State.ZoomIn;
+		if (currentState != State.StoppedIn) {
+			currentState = State.ZoomIn;
+			transitionTimer.Reset();
+		}
 	}
 
 	public void ZoomOut () {
-		currentState = State.ZoomOut;
+		if (currentState != State.StoppedOut) {
+			currentState = State.ZoomOut;
+			transitionTimer.Reset();
+		}
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		switch (currentState) {
 			case State.ZoomIn:
-				transform.position = Vector3.Lerp(transform.position, zoomPosition, 0.1f);
-				if ((transform.position - zoomPosition).sqrMagnitude < 0.1f) {
-					currentState = State.Stop;
+				transform.position = Vector3.Lerp(defaultPosition, zoomPosition, transitionTimer.PercentTimeLeft);
+				GetComponent<Camera>().fieldOfView = Mathf.Lerp(defaultFov, zoomFov, transitionTimer.PercentTimeLeft);
+				if (transitionTimer.IsOffCooldown) {
+					currentState = State.StoppedIn;
 				}
 				break;
 			case State.ZoomOut:
-				transform.position = Vector3.Lerp(transform.position, defaultPosition, 0.1f);
-				if ((transform.position - defaultPosition).sqrMagnitude < 0.1f) {
-					currentState = State.Stop;
+				transform.position = Vector3.Lerp(zoomPosition, defaultPosition, transitionTimer.PercentTimeLeft);
+				GetComponent<Camera>().fieldOfView = Mathf.Lerp(zoomFov, defaultFov, transitionTimer.PercentTimeLeft);
+				if (transitionTimer.IsOffCooldown) {
+					currentState = State.StoppedOut;
 				}
 				break;
-			case State.Stop:
+			default:
 				break;
 		}
 	}
--- Assets/Resources/Scripts/Timer.cs
+++ Assets/Resources/Scripts/Timer.cs
@@ -13,6 +13,7 @@ public class Timer {
 	}
 	public float TimeLeft { get { return cooldownTime - (Time.time - prevActivationTime); } }
 	public bool IsOffCooldown { get { return Time.time - prevActivationTime > cooldownTime; } }
+	public float PercentTimeLeft { get { return 1f - TimeLeft / cooldownTime; } }
 	public float CooldownTime { get { return cooldownTime; } set { cooldownTime = value; } }
 	public void Reset() { prevActivationTime = Time.time; }
 }
